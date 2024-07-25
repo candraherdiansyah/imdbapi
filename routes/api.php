abaa1f0541a215dbd1e5eccf143a9ea0a9e18d11
candra <candraherdiansyah14@gmail.com>
2024-07-25 11:01:37 +0700

    host

---
+++ routes/api.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\Api\AktorController;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\FilmController;
+use App\Http\Controllers\Api\GenreController;
+use App\Http\Controllers\Api\KategoriController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+// route kategori
+// Route::get('kategori', [KategoriController::class, 'index']);
+// Route::post('kategori', [KategoriController::class, 'store']);
+// Route::get('kategori/{id}', [KategoriController::class, 'show']);
+// Route::put('kategori/{id}', [KategoriController::class, 'update']);
+// Route::delete('kategori/{id}', [KategoriController::class, 'destroy']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::resource('kategori', KategoriController::class);
+    Route::resource('genre', GenreController::class);
+    Route::resource('aktor', AktorController::class);
+    Route::resource('film', FilmController::class);
+});
+
+// auth route
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
